cd60b21583af29bcd0789b40232b7b3cc0cdd9b6
Oskar Bergmann <osi23.78@gmail.com>
2018-09-24 21:30:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+	<head>
+		<title></title>
+		
+	<!-- znaczniki META -->
+		<meta charset="utf-8">
+		<meta name="description" content="Strona przeznaczona na lekcje WIA">
+		<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+	 <!-- CSS-->
+		<link rel="Stylesheet" type="text/css" href="css/reset.css" >
+		<link rel="Stylesheet" type="text/css" href="css/.css" >
+
+		
+	 <!--Skrypty-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		
+		
+	<!--Strona do generowania ikony  http://realfavicongenerator.net/  
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
+		-->
+
+	</head>
+	<body>
+		<header>
+		
+		</header>
+		<main>
+				
+		</main>
+		<footer>
+		
+		</footer>
+	</body>
+</html>
