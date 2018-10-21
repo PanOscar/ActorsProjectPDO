aad0bc042867adba8dd1163bf763cd26f5611723
OskarBerg <osi23.78@gmail.com>
2018-10-21 22:50:44 +0200

    z

--- connect.php
+++ connect.php
@@ -10,12 +10,11 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    
 ];
 try {
      $conn = new PDO($dsn, $dbuser, $dbpass, $options);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
- 
-
  ?>
